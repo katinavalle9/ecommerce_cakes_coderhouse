7f086a92b416a20dbc6dbf1494a5883b3784623a
Katia Valle <katinavalle9@gmail.com>
2023-11-06 18:46:58 -0600

    ultimos cambios

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "http://katinavalle9.github.io/ecommerce_cakes_coderhouse/",
+  base: "/ecommerce_cakes_coderhouse/",
   plugins: [react()],
 })
